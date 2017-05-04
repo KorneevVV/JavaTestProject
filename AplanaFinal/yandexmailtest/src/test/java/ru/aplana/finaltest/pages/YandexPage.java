252fa0157be1c424ff83ac3896006d79663fd59d
Dmitry Zotov <dmitriyzotov2009@yandex.ru>
2017-05-04 03:03:30 +0300

    Update YandexPage.java

--- AplanaFinal/yandexmailtest/src/test/java/ru/aplana/finaltest/pages/YandexPage.java
+++ AplanaFinal/yandexmailtest/src/test/java/ru/aplana/finaltest/pages/YandexPage.java
@@ -4,13 +4,8 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * Created by USERrr on 03.05.2017.
- */
+
 @DefaultUrl("https://www.yandex.ru/")
 public class YandexPage extends PageObject {
     @FindBy(xpath = "//a[@href='https://mail.yandex.ru']")
