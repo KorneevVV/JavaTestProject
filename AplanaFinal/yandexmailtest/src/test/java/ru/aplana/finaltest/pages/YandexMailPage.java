20facd5601b042bf04b287b7a6037dcca80ca155
Dmitry Zotov <dmitriyzotov2009@yandex.ru>
2017-05-04 03:02:57 +0300

    Update YandexMailPage.java

--- AplanaFinal/yandexmailtest/src/test/java/ru/aplana/finaltest/pages/YandexMailPage.java
+++ AplanaFinal/yandexmailtest/src/test/java/ru/aplana/finaltest/pages/YandexMailPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertTrue;
+
 @DefaultUrl("https://mail.yandex.ru/?uid=494578487&login=dmitriyzotovtest#inbox")
 public class YandexMailPage extends PageObject {
     @FindBy(xpath = "//a[contains(@class, 'mail-ComposeButton js-left-col-toolbar-compose')]")
@@ -35,7 +36,7 @@ public class YandexMailPage extends PageObject {
     @FindBy(xpath = "//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']")
     private WebElementFacade signLabel;
 
-    public void writeNewMail(String address, String sub, String text){
+    public void writeNewMail(String address, String sub, String text) {
         waiting("//a[contains(@class, 'mail-ComposeButton js-left-col-toolbar-compose')]");
         writeMailButton.click();
         addressField.clear();
@@ -45,30 +46,35 @@ public class YandexMailPage extends PageObject {
         mailTextField.type(text);
         sendMailButton.click();
     }
-    public void writeNewMailForChecking(){
+
+    public void writeNewMailForChecking() {
         waiting("//a[contains(@class, 'mail-ComposeButton js-left-col-toolbar-compose')]");
         writeMailButton.click();
     }
-    public void openSettings(){
+
+    public void openSettings() {
         waiting("//a[@class='mail-GhostButton js-header-settings nb']");
         settingsBtn.click();
     }
-    public void openUserDataSettings(){
+
+    public void openUserDataSettings() {
         waiting("(//span[@class='settings-popup-menu-item-content'])[1]");
         settingsButtonUserData.click();
     }
-    public String findSign(){
+
+    public String findSign() {
         return signLabel.getText();
     }
-    public void checkSign(String valueOfSign){
+
+    public void checkSign(String valueOfSign) {
         waiting("//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']");
-        assertTrue("sign = " + findSign() + "\n edited sign = "+valueOfSign
-                +"sign is not equals", findSign().equals("-- \n" + valueOfSign));
+        assertTrue("sign = " + findSign() + "\n edited sign = " + valueOfSign
+                + "sign is not equals", findSign().equals("-- \n" + valueOfSign));
         //Яндекс добавлял к подписи "--\n" которые мы не прописываи когда редактировали подпись
         //поэтому тест тут валился
     }
 
-    private void waiting(String xpathElement){
+    private void waiting(String xpathElement) {
         WebDriverWait wait = new WebDriverWait(getDriver(), 30);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathElement)));
     }
